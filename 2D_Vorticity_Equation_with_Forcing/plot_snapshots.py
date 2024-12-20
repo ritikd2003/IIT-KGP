568e1786e91910a9673a6916ea1bd94d52fab4a1
Ritik Dubey <ritikd214@gmail.com>
2024-12-20 07:00:25 +0530

    Add files via upload

---
+++ 2D_Vorticity_Equation_with_Forcing/plot_snapshots.py
@@ -0,0 +1,72 @@
+"""
+Plot vorticity planes from joint analysis files.
+
+Usage:
+    plot_snapshots.py <files>... [--output=<dir>]
+
+Options:
+    --output=<dir>  Output directory [default: ./frames]
+
+"""
+
+import h5py
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+from dedalus.extras import plot_tools
+
+def main(filename, start, count, output):
+    """Save plot of vorticity for the given range of analysis writes."""
+
+    # Plot settings
+    task = 'vorticity'  # Only plot vorticity
+    scale = 2
+    dpi = 200
+    title_func = lambda sim_time: 't = {:.3f}'.format(sim_time)
+    savename_func = lambda write: 'vorticity_{:06}.png'.format(write)
+    # Layout
+    image = plot_tools.Box(4, 2)  # Aspect ratio (width, height)
+    pad = plot_tools.Frame(0.2, 0, 0, 0)
+    margin = plot_tools.Frame(0.2, 0.1, 0, 0)
+
+    # Create single-figure layout
+    mfig = plot_tools.MultiFigure(1, 1, image, pad, margin, scale)
+    fig = mfig.figure
+    # Plot writes
+    with h5py.File(filename, mode='r') as file:
+        for index in range(start, start+count):
+            # Build subfigure axes
+            axes = mfig.add_axes(0, 0, [0, 0, 1, 1])
+            # Call 3D plotting helper, slicing in time
+            dset = file['tasks'][task]
+            plot_tools.plot_bot_3d(dset, 0, index, axes=axes, title=task, even_scale=True, visible_axes=False)
+            # Add time title
+            title = title_func(file['scales/sim_time'][index])
+            title_height = 1 - 0.5 * mfig.margin.top / mfig.fig.y
+            fig.suptitle(title, x=0.45, y=title_height, ha='left')
+            # Save figure
+            savename = savename_func(file['scales/write_number'][index])
+            savepath = output.joinpath(savename)
+            fig.savefig(str(savepath), dpi=dpi)
+            fig.clear()
+    plt.close(fig)
+
+if __name__ == "__main__":
+
+    import pathlib
+    from docopt import docopt
+    from dedalus.tools import logging
+    from dedalus.tools import post
+    from dedalus.tools.parallel import Sync
+
+    args = docopt(__doc__)
+
+    output_path = pathlib.Path(args['--output']).absolute()
+    # Create output directory if needed
+    with Sync() as sync:
+        if sync.comm.rank == 0:
+            if not output_path.exists():
+                output_path.mkdir()
+    post.visit_writes(args['<files>'], main, output=output_path)
---
+++ 2D_Vorticity_Equation_with_Forcing/vorticity_script_with_periodicForcing.py
@@ -0,0 +1,93 @@
+"""
+this is a Dedalus script for simulating a 2D Vorticity equation with a periodic forcing.
+This script demonstrates solving a 2D periodic initial value problem. 
+It can be ran serially or in parallel, and uses the built-in analysis framework 
+to save data snapshots to HDF5 files. The 'plot_snapshots.py` script can be used 
+to produce plots from the saved data.
+The simulation should take about 10 cpu-minutes to run.
+
+
+To run and plot using e.g. 6 processes:
+    $ mpiexec -n 6 python3 shear_flow.py
+    $ mpiexec -n 6 python3 plot_snapshots.py snapshots/*.h5
+"""
+
+import numpy as np
+import dedalus.public as d3
+import logging
+logger = logging.getLogger(__name__)
+
+
+# Parameters
+Lx, Lz = 2 * np.pi, 2 * np.pi
+Nx, Nz = 128,128
+dealias = 3/2
+stop_sim_time = 4.0
+timestepper = d3.RK222
+max_timestep = 0.01
+dtype = np.float64
+# Bases
+coords = d3.CartesianCoordinates('x', 'z')
+dist = d3.Distributor(coords, dtype=dtype)
+xbasis = d3.RealFourier(coords['x'], size=Nx, bounds=(0, Lx), dealias=dealias)
+zbasis = d3.RealFourier(coords['z'], size=Nz, bounds=(0, Lz), dealias=dealias)
+
+# Fields
+s = dist.Field(name='s', bases=(xbasis,zbasis))
+psi = dist.Field(name='psi', bases=(xbasis,zbasis))
+w = dist.Field( name='w', bases=(xbasis,zbasis))
+tau = dist.Field(name='tau')
+F = dist.Field(name='F', bases=(xbasis,zbasis))
+
+# Substitutions
+nu = 1 
+x, z = dist.local_grids(xbasis, zbasis)
+ex, ez = coords.unit_vector_fields(dist)
+F['g'] = 0.5 * np.sin(x) * np.sin(z)
+
+# Problem
+problem = d3.IVP([w, psi, tau, s], namespace=locals())
+problem.add_equation("dt(w)- nu*lap(w) = - skew(grad(psi))@grad(w) +F")
+problem.add_equation("dt(s)- nu*lap(s) = - skew(grad(psi))@grad(s)")
+problem.add_equation("lap(psi) + tau - w = 0 ")
+problem.add_equation("integ(psi) = 0")
+
+
+# Solver
+solver = problem.build_solver(timestepper)
+solver.stop_sim_time = stop_sim_time
+
+
+
+# Initial conditions
+# Initial conditions
+w['g'] = np.random.uniform(-1, 1, w['g'].shape) * 10  # Uniform random initial condition for w
+s['g'] = np.random.uniform(-1, 1, s['g'].shape) * 10    # Uniform random initial condition for s
+
+
+# Analysis
+snapshots = solver.evaluator.add_file_handler('snapshots', sim_dt=0.1, max_writes=100)
+snapshots.add_task(w, name='vorticity')
+# CFL
+CFL = d3.CFL(solver, initial_dt=max_timestep, cadence=10, safety=0.2, threshold=0.1,
+             max_change=1.5, min_change=0.5, max_dt=max_timestep)
+CFL.add_velocity(d3.skew(d3.grad(psi)))
+
+# Flow properties
+flow = d3.GlobalFlowProperty(solver, cadence=10)
+flow.add_property((w)**2, name='w2')
+
+# Main loop
+try:
+    logger.info('Starting main loop')
+    while solver.proceed:
+        timestep = max_timestep#CFL.compute_timestep()
+        solver.step(timestep)
+        if (solver.iteration-1) % 10 == 0:
+            max_w = np.sqrt(flow.max('w2'))
+            logger.info('Iteration=%i, Time=%e, dt=%e, max(w)=%f' %(solver.iteration, solver.sim_time, timestep, max_w))
+except:
+    logger.error('Exception raised, triggering end of main loop.')
+    raise
+finally:
+    solver.log_stats()
